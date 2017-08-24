3a1c0f194b707d42e8b501b7e16ab0d5fa99fbdb
gresia <gresia.g@hcl.com>
2017-08-24 16:15:19 -0500

    initial commit

---
+++ src/com/hcl/Main.java
@@ -0,0 +1,14 @@
+package com.hcl;
+
+public class Main {
+
+	String name = "Gresia";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Main m = new Main();
+		System.out.println(m.name + " Hello World!");
+		
+	}
+}
